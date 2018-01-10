7c410828186d9f9fa74e17ab9f5a361fae0851e7
arifhidayat <tayadih.fira@gmail.com>
2018-01-10 22:37:08 +0700

    config index page

---
+++ app/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+		<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<![endif]-->
+		<meta name="description" content="">
+		<meta name="author" content="ScriptsBundle">
+		<title>Desain Web</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link rel="stylesheet" type="text/css" href="css/style.css?r=<?=time()?>">
+	</head>
+	<body>
+	Landing Page
+	</body>
+<html>	
